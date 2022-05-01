4ac76d6e8f6aaa7c67f4f2ad4dcac0f6d5c4fd03
E.Bilgin <46282905+E-Bilgin@users.noreply.github.com>
2022-05-01 15:36:15 +0200

    Update online_platform.py

--- online_platform.py
+++ online_platform.py
@@ -5,7 +5,7 @@ import pandas as pd
 #import plotly.express as px
 import io
 import time
-import hydralit_components as hc
+#import hydralit_components as hc
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as html
 #from  PIL import ImageChops
